eb2b91e466c8e6b91d8806c8e72171e620d1a9fe
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 18:25:48 +0800

    project done

--- Check_Palindrome_Number.py
+++ Check_Palindrome_Number.py
@@ -2,7 +2,17 @@
 
 # Pseudocode
 
-# make the number into a string
+def palindrome(number):
+# Convert the number to a word
+    string_number = str(number)
 # reverse and check if it is still equal
+    return string_number == string_number[::-1]
 # code for users input
+user_input = int(input("Enter a number: "))
 # if-else statement
+if palindrome(user_input):
+    print(f"the original number is {user_input}")
+    print(f"Yes. given number {user_input} is palindrome number")
+else:
+    print(f"the original number is {user_input}")
+    print(f"No. given number {user_input} is not palindrome numbe")
